58bfe97404d4f58b1b3c049509b4827b72307566
Mael <Mael@FLY2>
2017-03-16 14:01:08 -0300

    Primeira modificação

---
+++ Projeto_de_Teste/src/CODIGOS/Tela.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CODIGOS;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Mael
+ */
+public class Tela {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Olá Mundo ");
+    }
+}
